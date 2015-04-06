f152325439f40cb07ae21848ce591f0013b2d6e9
Basile, Ricardo <ricardomonteirobasile@hotmail.com>
2015-04-06 19:50:54 -0300

    Nivelamento e Vilão
    Ajuste

--- main.lua
+++ main.lua
@@ -3,9 +3,10 @@
 ------------------------------------------------------------------------------------------------------------
 
 display.setStatusBar(display.HiddenStatusBar)
-local hit = audio.loadSound('song_hit.mp3')
+local hit = audio.loadSound('song_hit.mp3')local hitCat = audio.loadSound('song_hit_cat.mp3')
 local ambianceSound = audio.loadStream('song_ambiance_forest.mp3')
 local int controlePonto = 0
+local int life = 5
 
 ------------------------------------------------------------------------------------------------------------
 -- BACKGROUND
@@ -113,7 +114,7 @@ end
 function imgHunterOne:tap(event)
 	audio.play(hit)
 	imgHunterOne.alpha = 0;
-	scoreCount.text = tostring(tonumber(scoreCount.text ) + 25 )
+	scoreCount.text = tostring(tonumber(scoreCount.text ) + 50 )
 	controlePonto = tonumber(scoreCount.text )
 	controleVelocidade()
 	print ( controlePonto )
@@ -122,7 +123,7 @@ end
 function imgHunterTwo:tap(event)
 	audio.play(hit)
 	imgHunterTwo.alpha = 0;
-	scoreCount.text = tostring(tonumber(scoreCount.text ) + 25 )
+	scoreCount.text = tostring(tonumber(scoreCount.text ) + 50 )
 	controlePonto = tonumber(scoreCount.text )
 	controleVelocidade()
 	print ( controlePonto )	
@@ -152,11 +153,12 @@ function animalOneDown()
 end
 
 function imgAnimalOne:tap(event)
-	audio.play(hit)
+	audio.play(hitCat)
 	imgAnimalOne.alpha = 0;
-	controleVelocidade()
+	controleVelocidade()	life = (life - 1)	if life == 0 then		print ( "GAME OVER!" )	end	
 end
-------------------------------------------------------------------------------------------------------------
+
+------------------------------------------------------------------------------------------------------------
 -- CONTROLE DA VELOCIDADE DE TRANSIÇÃO
 ------------------------------------------------------------------------------------------------------------
 local int timeVilaoOneUp = 4500;
@@ -180,92 +182,92 @@ function controleVelocidade()
 	end
 
 	if controlePonto > 50 and controlePonto < 100 then
-		timeVilaoOneUp 			= 4500 - 400;
-		timeVilaoOneDown 		= 4500 - 400;
-		timeVilaoTwoUp 			= 4000 - 400;
-		timeVilaoTwoDown 		= 3500 - 400;
-		timeAnimalOneUp 		= 4000 - 400;
-		timeAnimalOneDown 		= 4500 - 400;
-		print ( "Nivel 2" )
-	end
-
-	if controlePonto > 100 and controlePonto < 200 then
 		timeVilaoOneUp 			= 4500 - 500;
 		timeVilaoOneDown 		= 4500 - 500;
 		timeVilaoTwoUp 			= 4000 - 500;
 		timeVilaoTwoDown 		= 3500 - 500;
 		timeAnimalOneUp 		= 4000 - 500;
 		timeAnimalOneDown 		= 4500 - 500;
-		print ( "Nivel 3" )
+		print ( "Nivel 2" )
 	end
 
-	if controlePonto > 200 and controlePonto < 300 then
+	if controlePonto > 100 and controlePonto < 200 then
 		timeVilaoOneUp 			= 4500 - 600;
 		timeVilaoOneDown 		= 4500 - 600;
 		timeVilaoTwoUp 			= 4000 - 600;
 		timeVilaoTwoDown 		= 3500 - 600;
 		timeAnimalOneUp 		= 4000 - 600;
 		timeAnimalOneDown 		= 4500 - 600;
-		print ( "Nivel 4" )
+		print ( "Nivel 3" )
 	end
 
-	if controlePonto > 300 and controlePonto < 400 then
+	if controlePonto > 200 and controlePonto < 300 then
 		timeVilaoOneUp 			= 4500 - 700;
 		timeVilaoOneDown 		= 4500 - 700;
 		timeVilaoTwoUp 			= 4000 - 700;
 		timeVilaoTwoDown 		= 3500 - 700;
 		timeAnimalOneUp 		= 4000 - 700;
 		timeAnimalOneDown 		= 4500 - 700;
-		print ( "Nivel 5" )
+		print ( "Nivel 4" )
 	end
 
-	if controlePonto > 400 and controlePonto < 500 then
+	if controlePonto > 300 and controlePonto < 400 then
 		timeVilaoOneUp 			= 4500 - 800;
 		timeVilaoOneDown 		= 4500 - 800;
 		timeVilaoTwoUp 			= 4000 - 800;
 		timeVilaoTwoDown 		= 3500 - 800;
 		timeAnimalOneUp 		= 4000 - 800;
 		timeAnimalOneDown 		= 4500 - 800;
-		print ( "Nivel 6" )
+		print ( "Nivel 5" )
 	end
 
-	if controlePonto > 500 and controlePonto < 600 then
+	if controlePonto > 400 and controlePonto < 500 then
 		timeVilaoOneUp 			= 4500 - 900;
 		timeVilaoOneDown 		= 4500 - 900;
 		timeVilaoTwoUp 			= 4000 - 900;
 		timeVilaoTwoDown 		= 3500 - 900;
 		timeAnimalOneUp 		= 4000 - 900;
 		timeAnimalOneDown 		= 4500 - 900;
-		print ( "Nivel 7" )
+		print ( "Nivel 6" )
 	end
 
-	if controlePonto > 600 and controlePonto < 700 then
+	if controlePonto > 500 and controlePonto < 600 then
 		timeVilaoOneUp 			= 4500 - 1000;
 		timeVilaoOneDown 		= 4500 - 1000;
 		timeVilaoTwoUp 			= 4000 - 1000;
 		timeVilaoTwoDown 		= 3500 - 1000;
 		timeAnimalOneUp 		= 4000 - 1000;
 		timeAnimalOneDown 		= 4500 - 1000;
-		print ( "Nivel 8" )
+		print ( "Nivel 7" )
 	end
 
-	if controlePonto > 700 and controlePonto < 800 then
+	if controlePonto > 600 and controlePonto < 700 then
 		timeVilaoOneUp 			= 4500 - 1100;
 		timeVilaoOneDown 		= 4500 - 1100;
 		timeVilaoTwoUp 			= 4000 - 1100;
 		timeVilaoTwoDown 		= 3500 - 1100;
 		timeAnimalOneUp 		= 4000 - 1100;
 		timeAnimalOneDown 		= 4500 - 1100;
-		print ( "Nivel 9" )
+		print ( "Nivel 8" )
 	end
 
-	if controlePonto > 800 and controlePonto < 900 then
+	if controlePonto > 700 and controlePonto < 800 then
 		timeVilaoOneUp 			= 4500 - 1200;
 		timeVilaoOneDown 		= 4500 - 1200;
 		timeVilaoTwoUp 			= 4000 - 1200;
 		timeVilaoTwoDown 		= 3500 - 1200;
 		timeAnimalOneUp 		= 4000 - 1200;
 		timeAnimalOneDown 		= 4500 - 1200;
+		print ( "Nivel 9" )
+	end
+
+	if controlePonto > 800 and controlePonto < 900 then
+		timeVilaoOneUp 			= 4500 - 1300;
+		timeVilaoOneDown 		= 4500 - 1300;
+		timeVilaoTwoUp 			= 4000 - 1300;
+		timeVilaoTwoDown 		= 3500 - 1300;
+		timeAnimalOneUp 		= 4000 - 1300;
+		timeAnimalOneDown 		= 4500 - 1300;
 		if controlePonto == 850 then
 			vilaoLiderUp();
 			imgHunterLider:addEventListener("tap", imgHunterLider);
@@ -274,7 +276,7 @@ function controleVelocidade()
 		print ( "Nivel 10" )
 	end
 
-	if controlePonto > 900 and controlePonto < 1000 then
+	if controlePonto > 900 and controlePonto < 1100 then
 		timeVilaoOneUp 			= 4500 - 1300;
 		timeVilaoOneDown 		= 4500 - 1300;
 		timeVilaoTwoUp 			= 4000 - 1300;
@@ -284,7 +286,7 @@ function controleVelocidade()
 		print ( "Nivel 11" )
 	end
 
-	if controlePonto > 1000 and controlePonto < 1100 then
+	if controlePonto > 1100 and controlePonto < 1300 then
 		timeVilaoOneUp 			= 4500 - 1400;
 		timeVilaoOneDown 		= 4500 - 1400;
 		timeVilaoTwoUp 			= 4000 - 1400;
@@ -294,7 +296,7 @@ function controleVelocidade()
 		print ( "Nivel 12" )
 	end
 
-	if controlePonto > 1100 and controlePonto < 1200 then
+	if controlePonto > 1300 and controlePonto < 1500 then
 		timeVilaoOneUp 			= 4500 - 1500;
 		timeVilaoOneDown 		= 4500 - 1500;
 		timeVilaoTwoUp 			= 4000 - 1500;
@@ -304,7 +306,7 @@ function controleVelocidade()
 		print ( "Nivel 13" )
 	end
 
-	if controlePonto > 1200 and controlePonto < 1300 then
+	if controlePonto > 1500 and controlePonto < 1700 then
 		timeVilaoOneUp 			= 4500 - 1600;
 		timeVilaoOneDown 		= 4500 - 1600;
 		timeVilaoTwoUp 			= 4000 - 1600;
@@ -314,7 +316,7 @@ function controleVelocidade()
 		print ( "Nivel 14" )
 	end
 
-	if controlePonto > 1300 and controlePonto < 1400 then
+	if controlePonto > 1700 and controlePonto < 1900 then
 		timeVilaoOneUp 			= 4500 - 1700;
 		timeVilaoOneDown 		= 4500 - 1700;
 		timeVilaoTwoUp 			= 4000 - 1700;
@@ -324,7 +326,7 @@ function controleVelocidade()
 		print ( "Nivel 15" )
 	end
 
-	if controlePonto > 1400 and controlePonto < 1500 then
+	if controlePonto > 1900 and controlePonto < 2100 then
 		timeVilaoOneUp 			= 4500 - 1800;
 		timeVilaoOneDown 		= 4500 - 1800;
 		timeVilaoTwoUp 			= 4000 - 1800;
@@ -334,7 +336,7 @@ function controleVelocidade()
 		print ( "Nivel 16" )
 	end
 
-	if controlePonto > 1500 and controlePonto < 1600 then
+	if controlePonto > 2100 and controlePonto < 2300 then
 		timeVilaoOneUp 			= 4500 - 1900;
 		timeVilaoOneDown 		= 4500 - 1900;
 		timeVilaoTwoUp 			= 4000 - 1900;
@@ -344,7 +346,7 @@ function controleVelocidade()
 		print ( "Nivel 17" )
 	end
 
-	if controlePonto > 1600 and controlePonto < 1700 then
+	if controlePonto > 2300 and controlePonto < 2500 then
 		timeVilaoOneUp 			= 4500 - 2000;
 		timeVilaoOneDown 		= 4500 - 2000;
 		timeVilaoTwoUp 			= 4000 - 2000;
@@ -354,7 +356,7 @@ function controleVelocidade()
 		print ( "Nivel 18" )
 	end
 
-	if controlePonto > 1700 and controlePonto < 1800 then
+	if controlePonto > 2500 and controlePonto < 2700 then
 		timeVilaoOneUp 			= 4500 - 2100;
 		timeVilaoOneDown 		= 4500 - 2100;
 		timeVilaoTwoUp 			= 4000 - 2100;
@@ -364,13 +366,13 @@ function controleVelocidade()
 		print ( "Nivel 19" )
 	end
 
-	if controlePonto > 1800 and controlePonto > 1900 then
-		timeVilaoOneUp 			= 4500 - 2200;
-		timeVilaoOneDown 		= 4500 - 2200;
-		timeVilaoTwoUp 			= 4000 - 2200;
-		timeVilaoTwoDown 		= 3500 - 2200;
-		timeAnimalOneUp 		= 4000 - 2200;
-		timeAnimalOneDown 		= 4500 - 2200;
+	if controlePonto > 2700 then
+		timeVilaoOneUp 			= 4500 - 2300;
+		timeVilaoOneDown 		= 4500 - 2300;
+		timeVilaoTwoUp 			= 4000 - 2300;
+		timeVilaoTwoDown 		= 3500 - 2300;
+		timeAnimalOneUp 		= 4000 - 2300;
+		timeAnimalOneDown 		= 4500 - 2300;
 		print ( "Nivel 20" )
 	end
 
